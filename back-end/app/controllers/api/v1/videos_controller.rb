8f0248088fec180dce6e4843670316419e0fa1ac
Hippoboy <hippoboy08@gmail.com>
2021-07-12 08:19:07 +0700

    Implemented API routes for videos management
    # base route: /api/v1
    - GET /videos : get all videos
    - GET /videos/:id: get video by Id
    - POST /videos: create new video
    - DELETE /videos/:id: delete video by Id

--- back-end/app/controllers/api/v1/videos_controller.rb
+++ back-end/app/controllers/api/v1/videos_controller.rb
@@ -5,25 +5,49 @@ class Api::V1::VideosController < ApplicationController
   # GET /videos.json
   def index
     @videos = Video.all
-    render json: @videos
+    if @videos
+      render json: { 
+        status: 'SUCCESS', 
+        message: 'Load successfully!', 
+        data: @videos 
+      }, status: :ok
+    else
+      render json: { 
+        status: 'ERROR', 
+        message: 'Failed to load videos!', 
+      }, status: :unprocessable_entity
+    end
   end
 
   # GET /videos/1
   # GET /videos/1.json
   def show
-    render json: @video
+    if @video
+      render json: { 
+        status: 'SUCCESS', 
+        message: 'Load successfully!', 
+        data: @video 
+      }, status: :ok
+    else
+      render json: { 
+        status: 'ERROR', 
+        message: 'Video not found!', 
+      }, status: :unprocessable_entity
+    end
   end
 
   # POST /videos
   # POST /videos.json
   def create
-    @video = Video.new(video_params)
+    @video = Video.new
+    @video.name = params[:name]
+    @video.thumbnail = params[:thumbnail]
+    @video.media = params[:media]
 
     if @video.save
-      # render :show, status: :created, location: @video
       render json: { 
         status: 'SUCCESS', 
-        message: 'Save video successfully!', 
+        message: 'Saved successfully!', 
         data: @video 
       }, status: :ok
     else
@@ -35,26 +59,53 @@ class Api::V1::VideosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /videos/1
-  # PATCH/PUT /videos/1.json
-  def update
-    if @video.update(video_params)
-      render :show, status: :ok, location: @video
-    else
-      render json: @video.errors, status: :unprocessable_entity
-    end
-  end
+  # # PATCH/PUT /videos/1
+  # # PATCH/PUT /videos/1.json
+  # def update
+  #   if @video.update(video_params)
+  #     render :show, status: :ok, location: @video
+  #   else
+  #     render json: @video.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /videos/1
   # DELETE /videos/1.json
   def destroy
-    @video.destroy
+    # Delete video by id if exists
+    isExisted = Video.exists?(:id => params[:id])
+    if isExisted
+      @video = Video.find(params[:id])
+      puts @video, isExisted
+      if @video.destroy
+        render json: { 
+          status: 'SUCCESS', 
+          message: 'Deleted successfully!'
+        }, status: :ok
+      else
+        render json: { 
+        status: 'ERROR', 
+        message: 'Failed to delete video!',
+        data: @video.errors
+      }, status: :unprocessable_entity
+      end
+    else
+      render json: { 
+        status: 'ERROR', 
+        message: 'Video not found!', 
+      }, status: :unprocessable_entity
+    end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_video
-      @video = Video.find(params[:id])
+      videoId = params[:id]
+      if Video.exists?(:id => videoId)
+        @video = Video.find(videoId)
+      else
+        @video = nil
+      end
     end
 
     # Only allow a list of trusted parameters through.
--- back-end/app/models/video.rb
+++ back-end/app/models/video.rb
@@ -1,2 +1,4 @@
 class Video < ApplicationRecord
+  mount_uploader :thumbnail, MediaUploader
+  mount_uploader :media, MediaUploader
 end
